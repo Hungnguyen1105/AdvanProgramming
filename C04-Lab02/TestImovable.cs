a1a4194e15fb0a8b1eeb9129384ea14e37ddd5a7
Hungnguyen1105 <hbday2k3@gmail.com>
2023-09-25 21:57:28 +0700

    Update TestImovable.cs
    update test

--- C04-Lab02/TestImovable.cs
+++ C04-Lab02/TestImovable.cs
@@ -8,5 +8,25 @@ namespace C01AP.C04_Lab02
 {
     internal class TestImovable
     {
+       public static void Main(string[] args)
+        {
+            IMovable m1 = new MovablePoint(5, 6, 10, 15);  // upcast 
+            Console.WriteLine(m1);
+            m1.moveLeft();
+            Console.WriteLine(m1);
+
+            Console.WriteLine("************************************");
+            IMovable m2 = new MovableCircle(1, 2, 3, 4, 20); // upcast	 
+            Console.WriteLine(m2);
+            m2.moveRight();
+            Console.WriteLine(m2);
+
+            Console.WriteLine("************************************");
+
+            MovableRectangle rectangle = new MovableRectangle(5, 5, 6, 6, 7, 7);
+            rectangle.moveUp();
+            rectangle.moveLeft();
+            Console.WriteLine(rectangle);
+        }
     }
 }
