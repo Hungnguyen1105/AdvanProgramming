635c003896fb2da9b9536c12f986219bcdd0cd0d
Hungnguyen1105 <hbday2k3@gmail.com>
2023-09-25 22:07:03 +0700

    Create Class1.cs

---
+++ C05-Lab05/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C01AP.AdvanProgramming.C05_Lab05
+{
+    internal class Class1
+    {
+    }
+}
