a12fde641a50e212608d4f8a648e820d758f6671
Hungnguyen1105 <hbday2k3@gmail.com>
2023-10-22 23:55:18 +0700

    fail copysingle
    error

---
+++ Assignment2/Factory/MultifunctionFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C01AP.AdvanProgramming.Assignment2.Factory
+{
+    class MultifunctionFactory : IAbstractFactory
+    {
+        public IAbstractFactory createCopy()
+        {
+            return new MultifunctionFactory();
+        }
+
+        public IAbstractFactory createPrint() 
+        {
+            return new MultifunctionFactory();
+
+        }
+        
+
+    }
+}
---
+++ Assignment2/Factory/SingleFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C01AP.AdvanProgramming.Assignment2.Factory
+{
+    class SingleFactory : IAbstractFactory
+    {
+        public IAbstractFactory createCopy()
+        {
+            return new SingleFactory();
+        }
+
+        public IAbstractFactory createPrint()
+        {
+            return new SingleFactory();
+        }
+    }
+}
